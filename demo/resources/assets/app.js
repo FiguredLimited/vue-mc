d0981b99806df021638796278fc5564eef16d76b
Rudi Theunissen <rtheunissen@platformscience.com>
2019-08-12 18:01:21 -0700

    Update demo packages

--- demo/resources/assets/app.js
+++ demo/resources/assets/app.js
@@ -3,8 +3,11 @@ import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.component('task-list', require('./components/TaskList.vue'));
-Vue.component('task', require('./components/Task.vue'));
+import TaskList from './components/TaskList.vue'
+import Task     from './components/Task.vue'
+
+Vue.component('task-list', TaskList);
+Vue.component('task', Task);
 
 Vue.use(ElementUI, {locale});
 
